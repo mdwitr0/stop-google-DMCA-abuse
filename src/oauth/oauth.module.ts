7da67db933c35906ea0e3fd4b1eed6d300378e93
Eugene Fedoryachenko <mdwit0r@gmail.com>
2022-05-07 22:28:07 +0200

    ✨ feat: add oauth controller

--- src/oauth/oauth.module.ts
+++ src/oauth/oauth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OauthService } from './oauth.service';
+import { OauthController } from './oauth.controller';
 
 @Module({
-  providers: [OauthService]
+  providers: [OauthService],
+  controllers: [OauthController]
 })
 export class OauthModule {}
