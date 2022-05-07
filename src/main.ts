a62be596570d3f91cb9adf58e3c8cf906216cb36
Eugene Fedoryachenko <mdwit0r@gmail.com>
2022-05-07 23:45:50 +0200

    ✨ feat: listen host & port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+(async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  const logger = new Logger('MAIN');
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  await app
+    .listen(configService.get('SERVER_PORT'), configService.get('SERVER_HOST'))
+    .then(() => {
+      logger.verbose(`
+      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+         Env: ${configService.get('APP_ENV')}
+         HTTP interface listen port ${configService.get('SERVER_PORT')}
+      ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+      `);
+    })
+    .catch((e) => {
+      logger.error(e);
+    });
+})();
