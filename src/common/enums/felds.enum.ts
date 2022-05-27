06dc0c91a56373e429013fef5c53801afc168513
mdwitr0 <mdwit0r@gmail.com>
2022-05-28 00:10:06 +0200

    ✨ feat: add fields enum

---
+++ src/common/enums/felds.enum.ts
@@ -0,0 +1,10 @@
+export enum FieldsEnum {
+  FULL_NAME = 'full_name',
+  YOUR_TITLE = 'your_title',
+  COMPANY_NAME = 'companyname',
+  CONTACT_EMAIL = 'contact_email_noprefill',
+  ADDRESS = 'address',
+  PHONE = 'phone',
+  DMCA_CLARIFICATIONS = 'dmca_clarifications',
+  SIGNATURE = 'signature',
+}
