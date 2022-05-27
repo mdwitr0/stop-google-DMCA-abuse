f27272064266c56b18a2176c929d79e44b6ad936
mdwitr0 <mdwit0r@gmail.com>
2022-05-28 01:58:06 +0200

    ✨ feat: add an entry to the message database

--- src/gmail/gmail.module.ts
+++ src/gmail/gmail.module.ts
@@ -1,9 +1,11 @@
 import { GmailService } from './gmail.service';
 import { Module } from '@nestjs/common';
 import { OauthModule } from 'src/oauth/oauth.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [GmailService],
-  imports: [OauthModule],
+  imports: [OauthModule, PrismaModule],
+  exports: [GmailService],
 })
 export class GmailModule {}
--- src/gmail/gmail.service.ts
+++ src/gmail/gmail.service.ts
@@ -1,14 +1,21 @@
-import * as fs from 'fs';
+import { Injectable, Logger } from '@nestjs/common';
 
-import { Injectable } from '@nestjs/common';
 import { OauthService } from 'src/oauth/oauth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { google } from 'googleapis';
 
 @Injectable()
 export class GmailService {
-  constructor(private readonly oauthService: OauthService) {}
+  private readonly logger = new Logger();
 
-  async getMessages(q: string) {
+  constructor(
+    private readonly oauthService: OauthService,
+    private readonly prisma: PrismaService,
+  ) {}
+
+  async findAndSaveMessages(q: string, nextPageToken?: string): Promise<any> {
+    this.logger.log(`Run: set messages`);
+
     const gmail = google.gmail({
       version: 'v1',
       auth: this.oauthService.client,
@@ -16,28 +23,52 @@ export class GmailService {
     const list = await gmail.users.messages.list({
       userId: 'me',
       q,
-      maxResults: 10000,
+      maxResults: 100,
+      pageToken: nextPageToken,
     });
 
-    const messages = [];
+    this.logger.log(
+      `Process: ${list.data.messages.length} messages found in the list`,
+    );
+
     for (const messageData of list.data.messages) {
-      const message = await gmail.users.messages.get({
-        userId: 'me',
-        id: messageData.id,
+      const savedMessage = await this.prisma.messages.findUnique({
+        where: { messageId: messageData.id },
       });
 
-      messages.push(
-        Buffer.from(
+      if (savedMessage) {
+        this.logger.debug(`Found message: ${messageData.id}`);
+      } else {
+        this.logger.debug(`Get message: ${messageData.id}`);
+
+        const message = await gmail.users.messages.get({
+          userId: 'me',
+          id: messageData.id,
+        });
+
+        const messageText = Buffer.from(
           message.data.payload.parts[0].body.data,
           'base64',
-        ).toString(),
-      );
+        ).toString();
+
+        const data = {
+          messageId: message.data.id,
+          text: messageText,
+        };
+
+        await this.prisma.messages.create({
+          data,
+        });
+
+        this.logger.debug(`Save message: ${messageData.id}`);
+      }
     }
 
-    return messages;
-  }
+    if (list.data.nextPageToken)
+      this.findAndSaveMessages(q, list.data.nextPageToken);
 
-  async onModuleInit() {
-    this.getMessages('sc-noreply@google.com');
+    this.logger.log(`Finish: all messages are saved`);
+
+    return { status: 'success' };
   }
 }
